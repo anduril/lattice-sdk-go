bceafcaf67df7812d29eb14ffcec110af9e414d7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-18 17:36:51 +0000

    SDK regeneration

--- objects.go
+++ objects.go
@@ -12,6 +12,8 @@ import (
 type GetObjectRequest struct {
 	// If set, Lattice will compress the response using the specified compression method. If the header is not defined, or the compression method is set to `identity`, no compression will be applied to the response.
 	AcceptEncoding *GetObjectRequestAcceptEncoding `json:"-" url:"-"`
+	// Indicates a client's preference for the priority of the response. The value is a structured header as defined in RFC 9218. If you do not set the header, Lattice uses the default priority set for the environment. Incremental delivery directives are not supported and will be ignored.
+	Priority *string `json:"-" url:"-"`
 }
 
 type ListObjectsRequest struct {
