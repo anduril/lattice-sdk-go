706e53ede9deb9b9b77c9c80ca291e021156bfb4
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-24 22:06:23 +0000

    SDK regeneration

--- core/request_option.go
+++ core/request_option.go
@@ -56,8 +56,8 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/anduril/lattice-sdk-go/v2")
-	headers.Set("X-Fern-SDK-Version", "v2.2.0")
-	headers.Set("User-Agent", "github.com/anduril/lattice-sdk-go/2.2.0")
+	headers.Set("X-Fern-SDK-Version", "v0.0.1444")
+	headers.Set("User-Agent", "github.com/anduril/lattice-sdk-go/2.2.1")
 	return headers
 }
 
